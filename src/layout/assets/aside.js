e806e8c0e9a9420cea36390af20e6aec153c7d1e
vinesk <vinesk.dev@gmail.com>
2024-03-07 10:54:56 +0100

    Rmove filters in aside, will do that later

--- src/layout/assets/aside.js
+++ src/layout/assets/aside.js
@@ -3,8 +3,8 @@ import section from "./section";
 export default function aside() {
   const aside = document.createElement("aside");
 
-  const filterSection = section("filters");
-  aside.appendChild(filterSection);
+  // const filterSection = section("filters");
+  // aside.appendChild(filterSection);
 
   const projectSection = section("projects");
   aside.appendChild(projectSection);
